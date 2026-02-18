668ef10c1255602e7d324f5050d86236ad9519dc
Mahesh <maheshawsj@gmail.com>
2026-02-18 23:40:31 +0530

    add

---
+++ roboshop.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+SG_ID="sg-0f4bc5cf41a204cc0"  # replace with your ID
+AMI_ID="ami-0220d79f3f480ecf5" 
+
+
+
+for instance in $@
+do
+   $( aws ec2 run-instances \
+    --image-id $AMI_ID \
+    --instance-type "t3.micro" \
+    --security-group-ids $SG_ID \
+    --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" \
+    --query 'Instances[0].InstanceId' \
+    --output text )
+
+    if [ $instance == "frontend" ];then
+IP=$(
+ aws ec2 describe-instances \
+ --instance-ids $instance_id \
+ --query 'Reseravtions[].Instances[].PublicIpAddress' \
+ --output text
+)
+else
+    IP=$(
+ aws ec2 describe-instances \
+ --instance-ids $instance_id \
+ --query 'Reseravtions[].Instances[].PrivateIpAddress' \
+ --output text )
+ fi
+done
+
